8b82868b6c4081d5ab5ea4200c415d7ca0c61481
id:001 <alexcarvajal@outlook.fr>
2024-07-14 02:29:58 -0400

    variables.js

---
+++ Variables.js
@@ -0,0 +1,58 @@
+// Variables and Storing Data
+
+/*
+    automaticaly
+    var
+    let
+    const
+
+*/
+var x=5;y=7;z=x+y; console.log(z)
+
+let a=4;b=2;c=a+b; console.log(c)
+
+const j=1;i=1;l=j+i; console.log(l)
+
+// One statement, many variables
+
+let person = "Juan Perez", edad = 48, trabajo = "programador";
+
+let animal = "orangutan",
+Signo = tauro,
+nickname = "waffles";
+
+
+// Re-Declaring Javascript Variables
+
+var castillo = "piedra";
+var castillo = "madera"
+
+// u cant declare variables using const or let
+
+// let waffles = "dulces";
+// let waffles = "salados"
+
+
+// Some arithmetics excercises
+
+let c = 5+3-2;
+
+// strings will me concatenated
+
+let x = "axel" + " " + "Mike"
+let q = "naranja" + 5 -3
+
+// Dollar sign is valid variable name
+
+let $$  = "hola mundo"
+let $$$ = 5
+let $ = $$ + $$$
+
+console.log($)
+
+// underscore are valid names too
+
+// for example var _name = "pedro";
+
+
+
